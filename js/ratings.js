0b82307b96bf2e49e724608cbb778d278a11fb7b
Nwaiwu Isidore <nwaiwuisidore@gmail.com>
2021-11-11 20:32:30 +0100

    Task 2 - 10

---
+++ js/ratings.js
@@ -0,0 +1,19 @@
+
+
+function collect_ratings(){
+    const elements = document.querySelectorAll(".rating");
+
+    const ratings = {
+        count: 0,
+        sum: 0,
+        average: 0
+    }
+    let rating = 0;
+
+    elements.forEach((element) => {
+        rating = parseInt(element.id.replace('star', ''));
+        ratings.count += parseInt(element.value);
+        ratings.sum += parseInt(element.value) * rating;
+    });
+    if(ratings.count !== 0){}
+}
